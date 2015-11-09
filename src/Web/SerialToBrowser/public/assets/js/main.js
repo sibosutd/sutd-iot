2d29183f386f3c1437adf8be1b19c404bbe40f43
Jia Hao <jiahaog@gmail.com>
2015-11-09 11:46:40 +0800

    Add base graph which responds to the server

--- src/Web/SerialToBrowser/public/assets/js/main.js
+++ src/Web/SerialToBrowser/public/assets/js/main.js
@@ -1,33 +1,66 @@
-// var label_0, serverResponse_0;	// UI elements
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    };
 
-function setup() {
-    label_0 = createSpan("Pulse:");	// the label
-    label_0.position(20,80);			// position it
-    serverResponse_0 = createSpan();	// create a div for server responses
-    serverResponse_0.position(label_0.width + 40, 80);	// position it
-    label_1 = createSpan("Oxygen:");	// the label
-    label_1.position(20,110);			// position it
-    serverResponse_1 = createSpan();	// create a div for server responses
-    serverResponse_1.position(label_1.width + 40, 110);	// position it
-    label_2 = createSpan("Patient position:");	// the label
-    label_2.position(20,140);			// position it
-    serverResponse_2 = createSpan();	// create a div for server responses
-    serverResponse_2.position(label_2.width + 40, 140);	// position it
-    getData(3);			// make a request back to the server
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
 }
 
-// this function makes a call to the server:
-function getData(channel) {
-    httpGet('/device/' + channel, update);
+function createCharts(selectors) {
+    return selectors.map(function (selector) {
+        var canvas = document.getElementById(selector);
+
+        var ctx = canvas.getContext('2d');
+
+        var startingData = {
+            labels: [1, 2, 3, 4, 5, 6, 7],
+            datasets: [
+                {
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+
+        // Reduce the animation steps for demo clarity.
+        return new Chart(ctx).Line(startingData, {animationSteps: 15});
+    });
 }
 
-// update the page when the server responds:
-function update(data) {
-    var arr = data.split(" ");
-    serverResponse_0.html(arr[0]);		// put the response in the span
-    serverResponse_1.html(arr[1]);
-    serverResponse_2.html(arr[2]);
-    // serverResponse_0.html(data);		// put the response in the span
-    getData(3);						// make another call to the server
+/**
+ * @param chart
+ * @param data x, y
+ */
+function addData(chart, data) {
+    console.log(data);
+    chart.addData([data[1]], data[0]);
+    // Remove the first point so we dont just add values forever
+    chart.removeData();
 }
-  		
+
+function updateCharts(charts, data) {
+    charts.forEach(function (chart, index) {
+        addData(chart, [counter, data[index]]);
+    });
+    counter += 1;
+}
+
+var CHART_NAMES = ['pulse-chart', 'oxygen-chart', 'patient-chart'];
+var CHANNEL = 3;
+var API_URL = '/device/' + CHANNEL;
+
+var charts = createCharts(CHART_NAMES);
+var counter = 8;
+
+setInterval(function () {
+    httpGetAsync(API_URL, function (rawData) {
+        var data = rawData.split(' ');
+        updateCharts(charts, data);
+    });
+}, 1000);
