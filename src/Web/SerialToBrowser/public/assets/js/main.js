e723c79003774c31adb7c9a8a75ef3379b43b90d
Jia Hao <jiahaog@gmail.com>
2015-11-09 15:22:58 +0800

    Create centered doughnut chart

--- src/Web/SerialToBrowser/public/assets/js/main.js
+++ src/Web/SerialToBrowser/public/assets/js/main.js
@@ -62,6 +62,42 @@ var COLORS = ['#2196F3', '#E91E63', '#009688'];
 var charts = createCharts(COLORS, CHART_NAMES);
 var counter = 8; // keeps track of the x-axis
 
+
+function createDoughnut(id) {
+    var canvas = document.getElementById(id);
+    var ctx = canvas.getContext('2d');
+    var data = [
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        }
+    ];
+
+    var myDoughnutChart = new Chart(ctx).Doughnut(data, {
+
+    });
+
+    return myDoughnutChart;
+}
+
+
+createDoughnut('pulse-doughnut');
+
+
 (function update() {
     httpGetAsync(API_URL, function (rawData) {
         var data = rawData.split(' ');
